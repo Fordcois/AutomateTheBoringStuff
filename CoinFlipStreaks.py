2beac51e4c52004b990375e96d9f91e49f34448f
Fordcois <63402941+Fordcois@users.noreply.github.com>
2020-04-13 15:19:05 +0100

    Update CoinFlipStreaks.py
    Completed Code - Now each sequence of flips will only add a 1 if it occured or a 0 if it did not - no multiple streaks from the same sequence

--- CoinFlipStreaks.py
+++ CoinFlipStreaks.py
@@ -1,53 +1,44 @@
 import random
-totalstreaks=0
-results=[0]
+totalstreaks = 0
+results = [0]
+streaktarget = 0
 
 def rlistgen(n):
     global results
-    results=results*(n)
+    results = results*(n)
 
 def trail(rl):
-    for i in range ((len(rl))):
+    for i in range((len(rl))):
         print(rl[i], end='')
 
-def toss():
-    for i in range (10):
-        if (random.randint(0,1))==1:
-            results[(i-1)]=('H')
+def toss(i):
+    for i in range(i):
+        if (random.randint(0, 1)) == 1:
+            results[(i-1)] = ('H')
         else:
-            results[(i-1)]=('T')
+            results[(i-1)] = ('T')
 
-def longstreak(fliplist):
-    currentstreak=1
-    higheststreak=0
-    streaknum=0
-    for i in range ((len(fliplist))-1):
-        if fliplist[i]==fliplist[(i+1)]:
-            currentstreak+=1
-            if currentstreak>= higheststreak:
-                higheststreak=currentstreak
-        else:
-            currentstreak=1
-    print (' - Highest streak was ' + (str(higheststreak)))
-
-def targetstreak (target,fliplist):
+def targetstreak(target, fliplist):
     global totalstreaks
-    currentstreak=1
-    streaknum=0
-    for i in range ((len(fliplist))-1):
-        if fliplist[i]==fliplist[(i+1)]:
-            currentstreak+=1
-            if currentstreak>= target:
-                streaknum+=1
+    global streaktarget
+    streaktarget = target
+    currentstreak = 1
+    streaknum = 0
+    for i in range((len(fliplist))-1):
+        if fliplist[i] == fliplist[(i+1)]:
+            currentstreak += 1
+            if currentstreak >= target:
+                streaknum += 1
+                break
         else:
-            currentstreak=1
-    totalstreaks+=streaknum
-    print ('A Streak of ' + (str(target)) + ' happened ' + (str(streaknum)) + ' times.' )
+            currentstreak = 1
+    totalstreaks += streaknum
 
-rlistgen(10)
-for i in range (5):
-    toss()
-    trail(results)
-    longstreak(results)
-    targetstreak(3,results)
-print ('A steak of 3 occured '+ (str(totalstreaks)) + ' times in total.')
+def experiment(numofflips, timesrepeated, streakaim):
+    rlistgen(numofflips)
+    for i in range(timesrepeated):
+        toss(numofflips)
+        targetstreak(streakaim, results)
+    print('A steak of ' + (str(streaktarget)) + ' occured ' + (str(totalstreaks)) + ' times in total across '+(str(numofflips*timesrepeated))+' coin flips')
+    print('According to this experiment there is a ' + (str((totalstreaks/timesrepeated)*100)) + ' percent chance of a streak of ' + (str(streaktarget)) + ' occuring in '+(str(numofflips)) + ' coin flips.')
+experiment(100, 10000, 6)
